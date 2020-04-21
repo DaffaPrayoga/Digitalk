1c2f4112a64a504d57a61c6f76f32d54541796d6
Daffa Prayoga <daffa.firdaus13@gmail.com>
2020-04-21 11:37:29 +0700

    Admin Dashboard + Logout

---
+++ resources/views/admin/admin_home.blade.php
@@ -0,0 +1,17 @@
+@section('head')
+    @include('layouts.head')
+@show
+<body class="font-regular">
+@section('loader')
+    @include('layouts.loader')
+@show
+@section('admin_nav')
+    @include('layouts.admin_nav')
+@show
+<div class="admin-body" style="margin-left: 260px;">
+
+</div>
+
+@section('js')
+    @include('layouts.js')
+@show
